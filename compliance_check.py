a24e86640e2c51addf7721925cdbeb095b89fc3b
Patrik Olsson <patrik.olsson@pfsw.com>
2017-12-14 10:29:43 +0100

    Adding first version of a compliance check script for CVP.

---
+++ compliance_check.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import cvp, optparse
+
+usage = 'usage: %prog [options]'
+op = optparse.OptionParser(usage=usage)
+op.add_option( '-c', '--cvphostname', dest='cvphostname', action='store', help='CVP host name FQDN or IP', type='string')
+op.add_option( '-u', '--cvpusername', dest='cvpusername', action='store', help='CVP username', type='string')
+op.add_option( '-p', '--cvppassword', dest='cvppassword', action='store', help='CVP password', type='string')
+op.add_option( '-d', '--container', dest='container', action='store', help='Name of container to use as root for compliance check', type='string')
+
+opts, _ = op.parse_args()
+
+host = opts.cvphostname
+user = opts.cvpusername
+password = opts.cvppassword
+containerName = opts.container
+
+server = cvp.Cvp( host )
+server.authenticate( user , password )
+
+container = server.getContainer(containerName)
+server.containerComplianceCheck(container)
